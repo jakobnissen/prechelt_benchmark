199a20341514bdb4df267f68b33c5513a17ef59c
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2021-08-05 12:48:22 +0200

    Optimize building dict

--- v2.jl
+++ v2.jl
@@ -6,7 +6,55 @@
 # - Mofified by: Jakob Nissen
 # - Date: 2021-07-24
 =#
+module t
+
+using BitIntegers
+
 const emptyStrings = String[]
+
+const DIGIT_LUT = let
+    arr = fill(0xff, 128)
+    for (n, letters) in enumerate([
+        ('e',),
+        ('j', 'n', 'q'),
+        ('r', 'w', 'x'),
+        ('d', 's', 'y'),
+        ('f', 't'),
+        ('a', 'm'),
+        ('c', 'i', 'v'),
+        ('b', 'k', 'u'),
+        ('l', 'o', 'p'),
+        ('g', 'h', 'z'),
+    ]), letter in letters
+        arr[UInt8(letter) + 0x01] = n - 1
+        arr[UInt8(uppercase(letter)) + 0x01] = n - 1
+    end
+    Tuple(arr)
+end
+
+function loadDictionary(file)::Dict{UInt256, Vector{String}}
+    dict = sizehint!(Dict{UInt256, Vector{String}}(), 1000)
+    for word in eachline(file)
+        push!(get!(dict, word_to_number(word)) do; String[] end, word)
+    end
+    dict
+end
+
+function word_to_number(word::Union{String, SubString{String}})
+    result = UInt256(1)
+    for b in codeunits(word)
+        if (b < 0x80) & ((b in UInt8('A'):UInt8('Z')) | (b in UInt8('a'):UInt8('z')))
+            result = result * 10 + byte_to_digit(b)
+        end
+    end
+    return result
+end
+
+function byte_to_digit(b::UInt8)
+    n = @inbounds DIGIT_LUT[b + 0x01]
+    n == 0xff && error("Not a letter")
+    return n
+end
 
 function printTranslations(io, num, digits, dict, start=1, words=String[])
     if start > ncodeunits(digits)
@@ -27,53 +75,21 @@ function printTranslations(io, num, digits, dict, start=1, words=String[])
     end
 end
 
-function loadDictionary(file)::Dict{BigInt, Vector{String}}
-    local dict = Dict{BigInt, Vector{String}}()
-    for word in eachline(file)
-        push!(get!(dict, wordToNumber(word)) do; String[] end, word)
-    end
-    dict
-end
-
 function nthDigit(digits::String, i::Int64)::UInt
     codeunit(digits, i) - UInt8('0')
 end
 
-function charToDigit(ch)
-    ch = lowercase(ch)
-    ch == 'e' && return 0
-    ch in ('j', 'n', 'q') && return 1
-    ch in ('r', 'w', 'x') && return 2
-    ch in ('d', 's', 'y') && return 3
-    ch in ('f', 't') && return 4
-    ch in ('a', 'm') && return 5
-    ch in ('c', 'i', 'v') && return 6
-    ch in ('b', 'k', 'u') && return 7
-    ch in ('l', 'o', 'p') && return 8
-    ch in ('g', 'h', 'z') && return 9
-    throw(DomainError(ch, "Not a letter"))
-end
-
-function wordToNumber(word::String)::BigInt
-    buf = IOBuffer()
-    print(buf, '1')
-    for ch in word
-        if isascii(ch) && isletter(ch)
-            print(buf, Char(charToDigit(ch)) + Int('0'))
-        end
-    end
-    parse(BigInt, String(take!(buf)))
-end
-
-function main(io::IO)
-    dict = open(isempty(ARGS) ? "dictionary.txt" : ARGS[begin]) do file
+function main(io::IO, p1, p2)
+    dict = open(p1) do file
         loadDictionary(file)
     end
 
-    open(length(ARGS) < 2 ? "input.txt" : ARGS[begin+1]) do file
+    open(p2) do file
         d = dict
         for num in eachline(file)
             printTranslations(io, num, filter(isdigit, num), d)
         end
     end
 end
+
+end # module
